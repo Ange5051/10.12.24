717ccd695b833b036d5c20a379b8116db027d05f
Ange5051 <147865401+Ange5051@users.noreply.github.com>
2024-12-10 13:42:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Отсортировать одномерный масив по возрастанию и по убыванию
+            string[] people = { "Tom", "Sam", "Bob", "Kate", "Tom", "Alice" };
+            Array.Sort(people);
+            foreach (var el in people)
+            {
+                Console.WriteLine(el);
+            }
+            Console.WriteLine("");
+            Array.Reverse(people);
+            foreach (var el in people)
+            {
+                Console.WriteLine(el);
+            }
+            Console.ReadKey();
+        }
+    }
+}
