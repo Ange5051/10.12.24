90beb60d4b5ab6eec493afb58961f094d818dac1
Ange5051 <147865401+Ange5051@users.noreply.github.com>
2024-12-10 14:07:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        /// <summary>
+        /// Подсчитать колличество числа со значением 5 в одномерном масиве
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int [] omas={5,7,1,9,5,3,6,5,5};
+            int count = 0;
+            foreach (var el in omas) 
+            {
+                if (el==5)
+                            {count++;}
+            }
+            Console.WriteLine(count);
+            Console.ReadKey();
+
+        }
+    }
+}
